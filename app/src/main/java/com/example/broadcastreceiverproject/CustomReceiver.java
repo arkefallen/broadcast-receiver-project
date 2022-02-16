e526e13e419868c6615c7d1be8053d72b679f75a
arkefallen <rafa9011@gmail.com>
2022-02-16 18:58:28 +0700

    add all files to github

---
+++ app/src/main/java/com/example/broadcastreceiverproject/CustomReceiver.java
@@ -0,0 +1,34 @@
+package com.example.broadcastreceiverproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+    String msg = "";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        // Untuk mengetahui event apa yang terjadi
+        switch (intent.getAction()) {
+            case Intent.ACTION_POWER_CONNECTED:
+                msg = "The power is connected";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                msg = "The power is disconnected";
+                break;
+            case "ACTION_CUSTOM_BROADCAST":
+                msg = "This is custom broadcast";
+                break;
+            default:
+                break;
+        }
+
+        // Memunculkan pesan/notifikasi
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/broadcastreceiverproject/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.broadcastreceiverproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity<ACTION_CUSTOM_BROADCAST> extends AppCompatActivity {
+    Button sendButton;
+    private String ACTION_CUSTOM_BROADCAST = "ACTION_CUSTOM_BROADCAST";
+
+    // Bikin broadcast
+    private static CustomReceiver customReceiver = new CustomReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Definisikan trigger nya pakai intent filter
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        // Daftarkan dulu customreceiver kita dengan intent filter yang akan mentrigger
+        registerReceiver(customReceiver,intentFilter);
+
+        sendButton = findViewById(R.id.send_btn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent broadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+                LocalBroadcastManager.getInstance(v.getContext()).sendBroadcast(broadcastIntent);
+            }
+        });
+
+        // Definisikan custom boradcast kita dengan intent filter
+        IntentFilter customIntent = new IntentFilter(ACTION_CUSTOM_BROADCAST);
+
+        // Daftarkan custom broadcast menggunakan LocalBroadCastManager
+        LocalBroadcastManager.getInstance(this).registerReceiver(customReceiver,customIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(customReceiver);
+    }
+}
